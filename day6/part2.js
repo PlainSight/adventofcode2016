fa5fd05f87d6a12bd8c534e45be60d4ab08ad17d
Alex <alexander.ogier@gmail.com>
2021-06-27 20:45:59 +1200

    day6

---
+++ day6/part2.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var mostCommonInPos = [];
+
+for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < input[i].length; j++) {
+        if (!mostCommonInPos[j]) {
+            mostCommonInPos.push({});
+        }
+
+        var letter = input[i][j];
+
+        if(mostCommonInPos[j][letter]) { 
+            mostCommonInPos[j][letter]++ 
+        } else { 
+            mostCommonInPos[j][letter] = 1; 
+        };
+    }
+}
+
+var result = '';
+
+for (var j = 0; j < mostCommonInPos.length; j++) {
+    result += Object.entries(mostCommonInPos[j]).sort((a, b) => a[1] - b[1])[0][0];
+}
+
+console.log(result);

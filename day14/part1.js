466f9be15aea8b7df20f1041a9d190d94de78f73
Alex <alexander.ogier@gmail.com>
2021-06-29 23:46:50 +1200

    day 14

--- day14/part1.js
+++ day14/part1.js
@@ -1,3 +1,26 @@
 var fs = require('fs');
+var md5 = require('md5');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var salt = fs.readFileSync('./input.txt', 'utf8');
+
+var keysFound = 0;
+
+outer: for(var i = 0; ; i++) {
+    var hash = md5(salt+i);
+    if(/(.)\1\1/.test(hash)) {
+        var char = /(.)\1\1/.exec(hash)[1];
+        var regex = new RegExp(char+char+char+char+char);
+        for(var j = 1; j <= 1000; j++) {
+            var innerhash = md5(salt+(i+j));
+            if (regex.test(innerhash)) {
+
+                keysFound++;
+                console.log(keysFound, i, hash, i+j, innerhash, char);
+                if (keysFound == 64) {
+                    return;
+                }
+                continue outer;
+            }
+        }
+    }
+}
--- day14/part2.js
+++ day14/part2.js
@@ -1,3 +1,42 @@
 var fs = require('fs');
+var md5 = require('md5');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var salt = fs.readFileSync('./input.txt', 'utf8');
+
+var keysFound = 0;
+
+var hashCache = {};
+
+function manyHash(v) {
+    if (hashCache[v]) {
+        return hashCache[v];
+    }
+    var h = md5(salt+v);
+    for(var i = 0; i < 2016; i++) {
+        h = md5(h);
+    }
+    hashCache[v] = h;
+    return h;
+}
+
+
+
+outer: for(var i = 0; ; i++) {
+    var hash = manyHash(i);
+    if(/(.)\1\1/.test(hash)) {
+        var char = /(.)\1\1/.exec(hash)[1];
+        var regex = new RegExp(char+char+char+char+char);
+        for(var j = 1; j <= 1000; j++) {
+            var innerhash = manyHash(i+j);
+            if (regex.test(innerhash)) {
+
+                keysFound++;
+                console.log(keysFound, i, hash, i+j, innerhash, char);
+                if (keysFound == 64) {
+                    return;
+                }
+                continue outer;
+            }
+        }
+    }
+}

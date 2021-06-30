544cc02178fd55067b95e4f5475e19c4a3e5dc37
Alex <alexander.ogier@gmail.com>
2021-07-01 00:05:44 +1200

    day 17 and 18

--- day17/part2.js
+++ day17/part2.js
@@ -1,3 +1,40 @@
 var fs = require('fs');
+var md5 = require('md5');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var passcode = fs.readFileSync('./input.txt', 'utf8')
+
+var longestPath = '';
+
+function tryPath(x, y, prior) {
+    if (x == 3 && y == 3) {
+        if (prior.length > longestPath.length) {
+            longestPath = prior;
+        }
+        return;
+    }
+
+    var hash = md5(passcode+prior);
+    var up = /[b-f]/.test(hash[0]) && y > 0;
+    var down = /[b-f]/.test(hash[1]) && y < 3;
+    var left = /[b-f]/.test(hash[2]) && x > 0;
+    var right = /[b-f]/.test(hash[3]) && x < 3;
+
+    //console.log(x, y, prior, up, down, left, right);
+
+    if (up) {
+        tryPath(x, y-1, prior+'U');
+    }
+    if (down) {
+        tryPath(x, y+1, prior+'D');
+    }
+    if (left) {
+        tryPath(x-1, y, prior+'L');
+    }
+    if (right) {
+        tryPath(x+1, y, prior+'R');
+    }
+}
+
+tryPath(0, 0, '');
+
+console.log(longestPath.length);
--- day18/part2.js
+++ day18/part2.js
@@ -1,3 +1,45 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8');
+
+var lastRow = input;
+
+var width = input.length;
+var wm = width-1;
+
+var safe = 0;
+
+safe += lastRow.split('').filter(a => a == '.').length;
+
+for (var i = 1; i < 400000; i++) {
+    var newRow = '';
+    for(var j = 0; j < width; j++) {
+        var trap = false;
+        //Its left and center tiles are traps, but its right tile is not.
+        if (j > 0 && lastRow[j-1] == '^' && lastRow[j] == '^' && (j == wm || lastRow[j+1] == '.')) {
+            trap = true;
+        }
+        
+        //Its center and right tiles are traps, but its left tile is not.
+        if (j < wm && lastRow[j] == '^' && lastRow[j+1] == '^' && (j == 0 || lastRow[j-1] == '.')) {
+            trap = true;
+        }
+
+        //Only its left tile is a trap.
+        if (j > 0 && lastRow[j-1] == '^' && lastRow[j] == '.' && (j == wm || lastRow[j+1] == '.')) {
+            trap = true;
+        }
+
+        //Only its right tile is a trap.
+        if (j < wm && lastRow[j] == '.' && lastRow[j+1] == '^' && (j == 0 || lastRow[j-1] == '.')) {
+            trap = true;
+        }
+        newRow += trap ? '^' : '.';
+        safe += trap ? 0 : 1;
+    }
+
+    lastRow = newRow;
+}
+
+console.log(safe);
+

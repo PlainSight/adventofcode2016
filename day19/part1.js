3a59e87f4d21d23d7c94529e20d052beff5402e2
Alex <alexander.ogier@gmail.com>
2021-07-03 17:01:17 +1200

    day 19

--- day19/part1.js
+++ day19/part1.js
@@ -1,3 +1,28 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var numberOfElves = parseInt(fs.readFileSync('./input.txt', 'utf8'));
+
+var firstElf = { number: 1, presents: 1, next: null };
+var lastElf = firstElf;
+
+for(var i = 2; i <= numberOfElves; i++) {
+    var newElf = { number: i, presents: 1, next: null };
+    lastElf.next = newElf;
+    lastElf = newElf;
+}
+
+lastElf.next = firstElf;
+
+var currentElf = firstElf;
+
+while(true) {
+    currentElf.presents += currentElf.next.presents;
+    currentElf.next = currentElf.next.next;
+
+    if (currentElf.presents == numberOfElves) {
+        console.log('done', currentElf.number);
+        return;
+    }
+
+    currentElf = currentElf.next;
+}

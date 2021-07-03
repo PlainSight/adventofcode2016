c49035621985c825c92bd5f9afd5755e8dfec2f5
Alex <alexander.ogier@gmail.com>
2021-07-03 17:01:17 +1200

    day 19

--- day19/part2.js
+++ day19/part2.js
@@ -1,3 +1,42 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var numberOfElves = parseInt(fs.readFileSync('./input.txt', 'utf8'));
+
+var firstElf = { number: 1, next: null, last: null };
+var lastElf = firstElf;
+
+var acrossElf = null;
+
+for(var i = 2; i <= numberOfElves; i++) {
+    var newElf = { number: i, next: null, last: lastElf };
+    lastElf.next = newElf;
+    lastElf = newElf;
+
+    if (i == Math.ceil((1+numberOfElves) / 2)) {
+        acrossElf = lastElf;
+    }
+}
+
+lastElf.next = firstElf;
+firstElf.last = lastElf;
+
+var currentElf = firstElf;
+
+var population = numberOfElves;
+
+while(currentElf.next != currentElf) {
+    //console.log('current', currentElf.number, 'next', currentElf.next.number, 'last', currentElf.last.number);
+    //console.log('killing', acrossElf.number, 'next', acrossElf.next.number, 'last', acrossElf.last.number);
+
+    acrossElf.last.next = acrossElf.next;
+    acrossElf.next.last = acrossElf.last;
+
+    if (population % 2 == 1) {
+        acrossElf = acrossElf.next;
+    }
+    acrossElf = acrossElf.next;
+    currentElf = currentElf.next;
+    population--;
+}
+
+console.log('done', currentElf);
